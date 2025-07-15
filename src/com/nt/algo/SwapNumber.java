d25b8c0ed1d79acd9b43d96a63c28ac002c63ccb
GautRepo <gautam.kr0352@gmail.com>
2025-07-15 18:21:46 +0530

    Initial work

---
+++ src/com/nt/algo/SwapNumber.java
@@ -0,0 +1,24 @@
+package com.nt.algo;
+
+import java.util.Scanner;
+
+public class SwapNumber {
+
+    void swap(int a, int b){
+        System.out.println("Before swap a & b "+a +" "+b);
+        int c = a;
+        a=b;
+        b=c;
+        System.out.println("After swap a & b "+a +" "+b);
+    }
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a =sc.nextInt();
+        System.out.println("Enter the value of b");
+        int b =sc.nextInt();
+        SwapNumber sn = new SwapNumber();
+        sn.swap(a,b);
+    }
+}
